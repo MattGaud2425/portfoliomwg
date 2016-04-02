f3b03e830656eb55d3032054093b21db88057e63
Matt Gaudio <mgaudio15@gmail.com>
2016-04-01 23:33:01 -0400

    Portfolio first commit

---
+++ config/initializers/assets.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w( search.js main.js )
+
+Rails.application.config.assets.precompile += %w( search.css main.css )
+
+# %w( controller_one controller_two controller_three ).each do |controller|
+#   Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+# end
