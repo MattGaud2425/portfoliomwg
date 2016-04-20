944243423974ffc7708c4b87bd782a263ef47d43
Matt Gaudio <mgaudio15@gmail.com>
2016-04-20 17:08:23 -0400

    added susy and made some nice adjustments

---
+++ app/assets/javascripts/projects.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$('.skills-btn').click(function() {
+		$('.skills').slideDown();
+		$('.skills').show();
+	};
+
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root "main#home"
 
+  get 'test' => 'main#test'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
