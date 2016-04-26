5cd73fbd42e9f0af20a574b932ae589fb97eebab
Matt Gaudio <mgaudio15@gmail.com>
2016-04-25 20:14:45 -0400

    production assets precompiled

---
+++ public/assets/main-235175b937682e27f7bdac1f3920b6ee423076a1b8fe4e36e138dcdcce31f87b.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".skills-btn").click(function(){$(".skills").fadeToggle("slow")}),$(".back-btn").hover(function(){$(".projects").fadeToggle("slow",function(){$(this).addClass("projects-rails")})})}),function(){}.call(this);
