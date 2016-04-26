9d047b9eb9d7962c4d7982433dbecda23ff4b14a
Matt Gaudio <mgaudio15@gmail.com>
2016-04-25 20:13:31 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  root "main#home"
+  root "main#test"
 
-  get 'test' => 'main#test'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
