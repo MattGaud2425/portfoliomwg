56e6d6f8523ece00d02291cba54308eeb7ec2324
Matt Gaudio <mgaudio15@gmail.com>
2016-08-24 15:37:39 -0400

    project pictures in place and looking decent

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 
-	$('.skills-btn').click(function() {
-		$('.skills').fadeToggle("slow");
-	});
+// 	$('.skills-btn').click(function() {
+// 		$('.skills').fadeToggle("slow");
+// 	});
 
-	$('.back-btn').hover(function() {
-		$('.projects').fadeToggle("slow", function() {
-			$(this).addClass('projects-rails');
-		});
-	});
+// 	$('.back-btn').hover(function() {
+// 		$('.projects').fadeToggle("slow", function() {
+// 			$(this).addClass('projects-rails');
+// 		});
+// 	});
 
-});
+// });
